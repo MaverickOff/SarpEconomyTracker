566dae3717c78e270e5a77255c52bcdfa50dbf77
MaverickOff <54652205+MaverickOff@users.noreply.github.com>
2024-07-10 18:31:56 +0200

    Optimimización de código

--- app/procesarRegistrosChat.php
+++ app/procesarRegistrosChat.php
@@ -1,7 +1,8 @@
 <?php
 include '../config/db_config.php';
 
-function insertar_saldo($conn, $saldo) {
+function insertar_saldo($conn, $saldo)
+{
     $query = "INSERT INTO `historial`(`id`, `saldo`, `fecha`) VALUES ('', $saldo, NOW())";
     $resultado = mysqli_query($conn, $query);
     if (!$resultado) {
@@ -11,13 +12,11 @@ function insertar_saldo($conn, $saldo) {
     return true;
 }
 
-
-
 $file = 'c:\Users\dougl\OneDrive\Documentos\GTA San Andreas User Files\SAMP\chatlog.txt';
 $ultima_coincidencia = '';
 
-if ( file_exists( $file ) ) {
-    $file_content = file_get_contents( $file );
+if (file_exists($file)) {
+    $file_content = file_get_contents($file);
 
     $cadena_banco = '/Banco: \\[\\$([\\d,]+)\\]/';
     $cadena_nuevo_balance = '/Nuevo balance: \\{FFFFFF\\}\\$([\\d,]+)/';
@@ -25,12 +24,12 @@ if ( file_exists( $file ) ) {
     $matches_banco = [];
     $matches_nuevo_balance = [];
 
-    preg_match_all( $cadena_banco, $file_content, $matches_banco, PREG_OFFSET_CAPTURE );
-    preg_match_all( $cadena_nuevo_balance, $file_content, $matches_nuevo_balance, PREG_OFFSET_CAPTURE );
+    preg_match_all($cadena_banco, $file_content, $matches_banco, PREG_OFFSET_CAPTURE);
+    preg_match_all($cadena_nuevo_balance, $file_content, $matches_nuevo_balance, PREG_OFFSET_CAPTURE);
 
-    $matches = array_merge( $matches_banco[ 1 ], $matches_nuevo_balance[ 1 ] );
+    $matches = array_merge($matches_banco[1], $matches_nuevo_balance[1]);
 
-    usort($matches, function($a, $b){
+    usort($matches, function ($a, $b) {
         return $a[1] - $b[1];
     });
     // Consultar último registro de la base de datos
@@ -38,7 +37,7 @@ if ( file_exists( $file ) ) {
     $ultimo_registro = mysqli_fetch_assoc($consulta_ultimo_registro);
 
     if (!empty($matches)) {
-        $procesar_saldo_actual = str_replace(',','',end($matches)[0]);
+        $procesar_saldo_actual = str_replace(',', '', end($matches)[0]);
         $saldo_actual = intval($procesar_saldo_actual);
         if ($ultimo_registro !== null) {
             $saldo_ultimo_registro = intval($ultimo_registro['saldo']);
@@ -48,7 +47,7 @@ if ( file_exists( $file ) ) {
             } else {
                 return $saldo_ultimo_registro;
             }
-        }else {
+        } else {
             echo "No se encontró ningún registro en la base de datos. :(";
             insertar_saldo($conn, $saldo_actual);
             return $saldo_actual;
@@ -64,10 +63,7 @@ if ( file_exists( $file ) ) {
             insertar_saldo($conn, $saldo_ultimo_registro);
             return $saldo_ultimo_registro;
         }
- }
+    }
 } else {
     echo "El archivo no existe";
 }
-
-
-?>
