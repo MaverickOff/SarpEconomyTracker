2b7f2a45cc535380629a89e87f4fcf1f70a2603e
MaverickOff <54652205+MaverickOff@users.noreply.github.com>
2024-07-13 04:44:09 +0200

    Se agrega una gráfica de prueba

--- public/index.php
+++ public/index.php
@@ -16,18 +16,46 @@ $mostrar_historial = include '../app/almacenarHistorialPagos.php';
 <body>
     <div class='contenedor-grid'>
         <div class='contenedor'>
-            Dinero: <span>$<?php echo $ultima_coincidencia;
-?></span>
+            Dinero: <span>$
+                <?php echo $ultima_coincidencia;
+                ?>
+            </span>
         </div>
         <div class='contenedor'>
             <h1>Historial</h1>
             <?php echo $mostrar_historial;
-?>
+            ?>
         </div>
-        <div class='contenedor'>3</div>
+        <div class='contenedor'>
+            <div>
+                <canvas id="estadisticas-sarp" width="15" height="5">
+            </div>
+            </canvas>
+        </div>
         <div class='contenedor'>4</div>
     </div>
 
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script>
+    const ctx = document.getElementById("estadisticas-sarp").getContext("2d");
+    const etiquetas = ["Enero", "Febrero", "Marzo", "Abril", "Mayo"];
+    const data = {
+        labels: etiquetas,
+        datasets: [{
+            label: "Dinero ganado",
+            data: [1000, 5600, 13200, 15555, 30000],
+            borderColor: "rgb(75, 192, 192)",
+            tension: 0.1,
+        }, ],
+    };
+
+    const config = {
+        type: "line",
+        data: data,
+    };
+
+    new Chart(ctx, config);
+    </script>
 </body>
 
 </html>
